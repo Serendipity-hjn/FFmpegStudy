77661b9cacc27d671153e7653309d4feb8487661
serendipity <976758426@qq.com>
2024-11-06 22:32:33 +0800

    options不设置为空出现错误

--- src/captureVideo.c
+++ src/captureVideo.c
@@ -1,11 +1,13 @@
 #include "libavformat/avformat.h"
 #include "libavutil/avutil.h"
 #include "libavcodec/avcodec.h"
+#include <libavutil/dict.h>
 #include <libavutil/log.h>
 #include <time.h>
 #include "libavdevice/avdevice.h"
 
 // 显示可用的摄像头设备
+// ffmpeg -f dshow -list_devices true -i dummy
 void avfoundationListDevices()
 {
     const AVInputFormat * inFmt = av_find_input_format("dshow");
@@ -13,9 +15,13 @@ void avfoundationListDevices()
     {
         av_log(NULL, AV_LOG_ERROR, "find dshow failed!\n");
     }
+    // 设置参数
+    AVDictionary *options;
+    av_dict_set(&options, "list_devices", "true", 0);
+
     AVFormatContext *inFmtCtx = avformat_alloc_context();
     // 第二个参数是URL
-    int ret = avformat_open_input(&inFmtCtx, "video=Integrated Camera", inFmt, NULL);
+    int ret = avformat_open_input(&inFmtCtx, "video=Integrated Camera", inFmt, &options);
     if (ret != 0)
     {
         av_log(NULL, AV_LOG_ERROR, "open input format failed:%s\n", av_err2str(ret));
