94cda1c09cbbdf51bc0960fa6078ff5d43c7d352
serendipity <976758426@qq.com>
2024-11-06 14:43:34 +0800

    显示可用摄像头

--- src/captureVideo.c
+++ src/captureVideo.c
@@ -1,1 +1,29 @@
 #include "libavformat/avformat.h"
+#include "libavutil/avutil.h"
+#include "libavcodec/avcodec.h"
+#include <libavutil/log.h>
+#include <time.h>
+
+// 显示可用的摄像头设备
+void avfoundationListDevices()
+{
+    const AVInputFormat * inFmt = av_find_input_format("dshow");
+    if (inFmt == NULL)
+    {
+        av_log(NULL, AV_LOG_ERROR, "find dshow failed!\n");
+    }
+    AVFormatContext *inFmtCtx = avformat_alloc_context();
+    // 第二个参数是URL
+    int ret = avformat_open_input(&inFmtCtx, "dummy", inFmt, NULL);
+    if (ret != 0)
+    {
+        av_log(NULL, AV_LOG_ERROR, "open input format failed:%s\n", av_err2str(ret));
+        return;
+    }
+}
+int main(int argc, char *argv[])
+{
+    av_log_set_level(AV_LOG_INFO);
+    avfoundationListDevices();
+    return 0;    
+}
